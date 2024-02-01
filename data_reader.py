6f22e2a6bd88d25f904afbcb26510c5013192dbd
Muhannad Al Ulaby <mual0001@stud.kea.dk>
2024-02-01 10:36:48 +0100

    further work with data parsing and html redirecting

--- data_reader.py
+++ data_reader.py
@@ -4,16 +4,18 @@ import csv
 import os
 import pandas as pd
 
+
 # read the csv file and return a list of lists
 def read_csv_file(file_name):
     data = []
-    with open(file_name, 'r') as csv_file:
+    with open(file_name, "r") as csv_file:
         reader = csv.reader(csv_file)
+        # skip the header
+        next(reader)
         for row in reader:
             data.append(row)
     return data
 
-output = read_csv_file('data_files/anna.csv')
+
+output = read_csv_file("data_files/anna.csv")
 print(output)
-
-
