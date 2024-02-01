bbf395aa9e008c77baf30e6ca4821edeb6483598
Muhannad Al Ulaby <mual0001@stud.kea.dk>
2024-02-01 09:42:02 +0100

    inital commit

---
+++ data_reader.py
@@ -0,0 +1,19 @@
+# parse the data from the csv file in the dta_files folder
+
+import csv
+import os
+import pandas as pd
+
+# read the csv file and return a list of lists
+def read_csv_file(file_name):
+    data = []
+    with open(file_name, 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
+            data.append(row)
+    return data
+
+output = read_csv_file('data_files/anna.csv')
+print(output)
+
+
