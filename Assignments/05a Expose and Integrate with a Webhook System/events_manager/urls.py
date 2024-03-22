db8295955d6526f8b0e4cd1d3f7b486b75ee1368
Muhannad Al Ulaby <mual0001@stud.kea.dk>
2024-03-22 16:52:37 +0100

    added auth endpoint

--- Assignments/05a Expose and Integrate with a Webhook System/events_manager/urls.py
+++ Assignments/05a Expose and Integrate with a Webhook System/events_manager/urls.py
@@ -1,17 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path(
-        "users/", include("users.urls")
-    ),  # Include the users app URLs under the 'api/' namespace
-    # Schema and Documentation paths
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),  # API schema
+    path("users/", include("users.urls")),
+    path("users/token/", obtain_auth_token, name="token_auth"),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/docs/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
-    ),  # Swagger UI
+    ),
 ]
