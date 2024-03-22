cb01194155c552e5a134b4541f0b65af3cce218a
Muhannad Al Ulaby <mual0001@stud.kea.dk>
2024-03-22 13:50:34 +0100

    created admin set up with super user

--- Assignments/05a Expose and Integrate with a Webhook System/users/admin.py
+++ Assignments/05a Expose and Integrate with a Webhook System/users/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    # Define admin model for custom User model with no email field.
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("email",)}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "email", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = ("username", "email", "is_staff")
+    search_fields = ("username", "email")
+    ordering = ("username",)
+
+
+admin.site.register(User, UserAdmin)
