1b9a37486c51382eeced872082d1b063c0e1e8da
Muhannad Al Ulaby <mual0001@stud.kea.dk>
2024-03-22 17:35:01 +0100

    added veiwsets to events app

--- Assignments/05a Expose and Integrate with a Webhook System/events/views.py
+++ Assignments/05a Expose and Integrate with a Webhook System/events/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Event, Topic
+from .serializers import EventSerializer, TopicSerializer
 
-# Create your views here.
+
+class TopicViewSet(viewsets.ModelViewSet):
+    queryset = Topic.objects.all()
+    serializer_class = TopicSerializer
+
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned events to a given topic,
+        by filtering against a `topic` query parameter in the URL.
+        """
+        queryset = Event.objects.all()
+        topic = self.request.query_params.get("topic")
+        if topic is not None:
+            queryset = queryset.filter(topic__name=topic)
+        return queryset
