1e6cf171ebe1ecb8abf4dfee3719baffe081400b
Muhannad Al Ulaby <mual0001@stud.kea.dk>
2024-03-22 14:29:14 +0100

    added views and routing logic in the users app

--- Assignments/05a Expose and Integrate with a Webhook System/users/views.py
+++ Assignments/05a Expose and Integrate with a Webhook System/users/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .serializers import UserSerializer
+from django.http import HttpResponse
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_decode
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+User = get_user_model()
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class VerifyEmailView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request, uidb64, token):
+        try:
+            uid = urlsafe_base64_decode(uidb64).decode()
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+
+        if user is not None and default_token_generator.check_token(user, token):
+            user.is_active = True
+            user.save()
+            return HttpResponse("Email verification successful!")
+        else:
+            return HttpResponse(
+                "Email verification failed.", status=status.HTTP_400_BAD_REQUEST
+            )
