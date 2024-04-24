490031c42cd44ccf1425b20be7911f744b86705b
Muhannad Al Ulaby <mual0001@stud.kea.dk>
2024-04-24 12:43:32 +0200

    added asgi setup to the project

--- Assignments/06a WebSocket Example/web_channels/asgi.py
+++ Assignments/06a WebSocket Example/web_channels/asgi.py
@@ -1,16 +1,15 @@
-"""
-ASGI config for web_channels project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
+# websocket_example/asgi.py
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from chat import routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_channels.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web_channels.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns)),
+    }
+)
