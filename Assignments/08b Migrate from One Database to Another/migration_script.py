0125a182c135f50b47cac26b3133589dd3249cec
Muhannad Al Ulaby <mual0001@stud.kea.dk>
2024-05-05 17:11:01 +0200

    reformatting the migration script

--- Assignments/08b Migrate from One Database to Another/migration_script.py
+++ Assignments/08b Migrate from One Database to Another/migration_script.py
@@ -6,43 +6,90 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Campus',
+            name="Campus",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=100)),
-                ('city', models.CharField(max_length=100)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=100)),
+                ("city", models.CharField(max_length=100)),
             ],
         ),
         migrations.CreateModel(
-            name='Major',
+            name="Major",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=100)),
-                ('campus', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='school.campus')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=100)),
+                (
+                    "campus",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE, to="school.campus"
+                    ),
+                ),
             ],
         ),
         migrations.CreateModel(
-            name='Course',
+            name="Course",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=100)),
-                ('major', models.ManyToManyField(related_name='courses', to='school.major')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=100)),
+                (
+                    "major",
+                    models.ManyToManyField(related_name="courses", to="school.major"),
+                ),
             ],
         ),
         migrations.CreateModel(
-            name='Student',
+            name="Student",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=100)),
-                ('dateOfBirth', models.DateField()),
-                ('gender', models.CharField(max_length=10)),
-                ('courses', models.ManyToManyField(related_name='students', to='school.course')),
-                ('major', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='school.major')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=100)),
+                ("dateOfBirth", models.DateField()),
+                ("gender", models.CharField(max_length=10)),
+                (
+                    "courses",
+                    models.ManyToManyField(related_name="students", to="school.course"),
+                ),
+                (
+                    "major",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE, to="school.major"
+                    ),
+                ),
             ],
         ),
     ]
