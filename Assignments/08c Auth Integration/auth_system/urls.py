ae04607518be8ddccb7ba60ab5e95f6b7522bc6b
Muhannad Al Ulaby <mual0001@stud.kea.dk>
2024-05-08 13:38:45 +0200

    added an interface soloution to the log in process

--- Assignments/08c Auth Integration/auth_system/urls.py
+++ Assignments/08c Auth Integration/auth_system/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path, include
 from django.contrib import admin
-# import the swagger schema and urls 
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
+# import the swagger schema and urls
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+from sso_integration.views import home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),  # This includes all the allauth URLs
-    #path("api/", include("sso_integration.urls")),
+    # path("api/", include("sso_integration.urls")),
+    path("", home, name="home"),
 ]
--- Assignments/08c Auth Integration/sso_integration/views.py
+++ Assignments/08c Auth Integration/sso_integration/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# myapp/views.py
 
-# Create your views here.
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+def home(request):
+    if request.user.is_authenticated:
+        # User is logged in, display their email
+        return HttpResponse(f"Welcome to the app, you have successfully logged in using {request.user.email}")
+    else:
+        # User is not logged in, prompt to login or sign up
+        return HttpResponse("Welcome to the Home Page! Please use /accounts/login/ to log in or sign up.")
+
